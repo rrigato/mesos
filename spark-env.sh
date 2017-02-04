ea7ca1ad5f0508bab2b9dc81faf516317341cd95
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2017-02-03 20:58:49 -0600

    Create spark-env.sh
    Basic spark environment shell script

---
+++ spark-env.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+#can't put a space in between the equal sign and the string literal
+export MESOS_NATIVE_JAVA_LIBRARY='/home/ryan/Documents/Mesos/mesos-1.1.0/build/src/.libs/libmesos.so'
