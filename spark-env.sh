bd74c7746a01abd807d60113aafb3ba33f67f3ee
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2017-02-19 12:35:33 -0600

    Update spark-env.sh
    Adding more environment variables for the version of Python I am using

--- spark-env.sh
+++ spark-env.sh
@@ -2,3 +2,7 @@
 
 #can't put a space in between the equal sign and the string literal
 export MESOS_NATIVE_JAVA_LIBRARY='/home/ryan/Documents/Mesos/mesos-1.1.0/build/src/.libs/libmesos.so'
+
+#setting the python work agent and driver
+export PYSPARK_PYTHON=/usr/bin/python3
+export PYSPARK_DRIVER_PYTHON=/usr/bin/python3
